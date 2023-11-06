007141ee35dd0efdb9f5b20343284e41863fbf8e
Xiang Li <github@x1ang.li>
2023-11-05 19:22:05 -0500

    Create lc_318.maximum-product-of-word-lengths.py

---
+++ lc_318.maximum-product-of-word-lengths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        ord_a = ord('a')
+        maskLenMap = defaultdict(int)
+        for word in words:
+            mask = 0
+            for c in list(word):
+                mask |= 1 << ord(c)-ord_a
+            maskLenMap[mask] = max(len(word), maskLenMap[mask])
+        
+        ans = 0
+        for mask1, mask2 in product(maskLenMap, repeat = 2):
+            if not mask1 & mask2:
+                ans = max(ans, maskLenMap[mask1] * maskLenMap[mask2])
+        
+        return ans
+    
